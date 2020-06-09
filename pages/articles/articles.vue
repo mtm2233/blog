a4fc64ae5c746caa2af8527e5c9c32ad5141d27d
youcann <2541817610@qq.com>
2020-06-09 19:05:45 +0800

    init

---
+++ pages/articles/articles.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="articles">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "articles",
+		data() {
+			return {}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
