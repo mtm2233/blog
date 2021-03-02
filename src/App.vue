9bff0fdc0cc9e742fa1def05cdc193fa5a8791fa
马天梦 <2541817610@qq.com>
2021-03-02 13:35:29 +0800

    Transfer

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Description:
+ * @Author: mTm
+ * @Date: 2021-03-02 13:33:18
+ * @LastEditTime: 2021-03-02 13:35:10
+ * @LastEditors: mTm
+-->
 <template>
   <div id="app">
     <router-view></router-view>
@@ -6,7 +13,7 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
 }
 </script>
 
